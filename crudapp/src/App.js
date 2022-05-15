69bb08f426f295370435fb3064ccba5af23f0193
lord-JesusChrist <obide1@yahoo.com>
2022-05-15 18:34:08 +0100

    a setup

---
+++ crudapp/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Component1 from './components/Component1';
+import {useState} from 'react';
+
+
+
+function App() {
+ 
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
---
+++ crudapp/src/components/Component1.js
@@ -0,0 +1,9 @@
+
+function Component1()
+{
+    return <>
+    <h1>Hellowo world </h1>
+    </>
+}
+
+export default Component1
